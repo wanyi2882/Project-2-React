261cddd73ac22b43f9ce04c5d84257442a3e2353
wanyi2882 <wanyi.2882@gmail.com>
2021-10-17 13:26:11 +0000

    Create profile form (with 2 way binding)

--- project-2/src/components/Florists.js
+++ project-2/src/components/Florists.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import axios from 'axios'
 
-export default class Listing extends React.Component {
+export default class Florists extends React.Component {
 
     url = 'https://3000-tan-nightingale-xhc8uhmi.ws-us18.gitpod.io'
 
     state = {
         'data': [
 
-        ]
+        ],
+        'newUserName': "",
+        'newLoginEmail': "",
+        'newFloristName': "",
+        'newContactCategory': [],
+        'newInstagramURL': "",
+        'newFacebookURL': "",
+        'newEmailAddress': "",
+        'newContactNumber': ""
     }
 
     fetchData = async () => {
@@ -18,19 +26,146 @@ export default class Listing extends React.Component {
         })
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.fetchData()
     }
 
-    render(){
+    updateFormField = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    updateCategoryCheckboxes = (event) => {
+        // check if the value that the user has clicked already exists
+        // 1. if exists, then the user is UNCHECKING the box
+        // 2. if does not exists, then the user is CHECKING the box
+
+        let arrayToModify = this.state[event.target.name];
+
+        if (arrayToModify.includes(event.target.value)) {
+            let indexToRemove = arrayToModify.indexOf(event.target.value);
+            let cloned = [...arrayToModify.slice(0, indexToRemove), 
+                          ...arrayToModify.slice(indexToRemove + 1)];
+            this.setState({
+                [event.target.name]: cloned
+            })
+        } else {
+            // clone the array
+            let cloned = [...arrayToModify, event.target.value];
+            this.setState({
+                [event.target.name]: cloned
+            })
+        }
+    }
+
+    render() {
         return <React.Fragment>
-            {this.state.data.map( florist => 
-            <div className="card" key={florist._id}>
-                <div className="card-body">
-                    <h3 className="card-title">{florist.name}</h3>
+            <h1>Create Account</h1>
+
+            <div>
+                <h3>Login Information</h3>
+                <div>
+                    <label className="form-label">User Name:</label>
+                    <input type="text"
+                        className="form-control"
+                        name="newUserName"
+                        value={this.state.newUserName}
+                        onChange={this.updateFormField} />
+                </div>
+                <div>
+                    <label className="form-label">Login Email:</label>
+                    <input type="text"
+                        className="form-control"
+                        name="newLoginEmail"
+                        value={this.state.newLoginEmail}
+                        onChange={this.updateFormField} />
+                </div>
+            </div>
+
+            <div>
+                <h3>More About You (Profile to Public)</h3>
+                <div>
+                    <label className="form-label">Name of Florist:</label>
+                    <input type="text"
+                        className="form-control"
+                        name="newFloristName"
+                        value={this.state.newFloristName}
+                        onChange={this.updateFormField} />
+                </div>
+                <div>
+                    <div><label className="form-label">Ways you want to be contacted thru: </label></div>
+                    <div className="form-check-inline">
+                        <input className="form-check-input" 
+                           type="checkbox" 
+                           value="whatsapp" 
+                           name="newContactCategory"
+                           checked={this.state.newContactCategory.includes('whatsapp')}
+                           onChange={this.updateCategoryCheckboxes} />
+                        <label className="form-check-label" for="category-whatsapp">
+                            Whatsapp
+                        </label>
+                    </div>
+
+                    <div className="form-check-inline">
+                        <input className="form-check-input" 
+                           type="checkbox" 
+                           value="call" 
+                           name="newContactCategory"
+                           checked={this.state.newContactCategory.includes('call')}
+                           onChange={this.updateCategoryCheckboxes} />
+                        <label className="form-check-label" for="category-call">
+                            Call
+                        </label>
+                    </div>
+
+                    <div className="form-check-inline">
+                        <input className="form-check-input" 
+                           type="checkbox" 
+                           value="email" 
+                           name="newContactCategory"
+                           checked={this.state.newContactCategory.includes('email')}
+                           onChange={this.updateCategoryCheckboxes} />
+                        <label className="form-check-label" for="category-email">
+                            Email
+                        </label>
+                    </div>                    
                 </div>
 
-            </div>)}
+                <div>
+                    <label className="form-label">Email Address (Please fill this field if you have ticked 'Email' above):</label>
+                    <input type="text"
+                        className="form-control"
+                        name="newEmailAddress"
+                        value={this.state.newEmailAddress}
+                        onChange={this.updateFormField} />
+                </div>
+                <div>
+                    <label className="form-label">Contact Number(Please fill this field if you have ticked 'Call' and/or 'Whatsapp' above):</label>
+                    <input type="text"
+                        className="form-control"
+                        name="newContactNumber"
+                        value={this.state.newContactNumber}
+                        onChange={this.updateFormField} />
+                </div>
+                <div>
+                    <label className="form-label">Instagram URL (If available):</label>
+                    <input type="text"
+                        className="form-control"
+                        name="newInstagramURL"
+                        value={this.state.newInstagramURL}
+                        onChange={this.updateFormField} />
+                </div>
+                <div>
+                    <label className="form-label">Facebook URL (If available):</label>
+                    <input type="text"
+                        className="form-control"
+                        name="newFacebookURL"
+                        value={this.state.newFacebookURL}
+                        onChange={this.updateFormField} />
+                </div>
+                
+            </div>
         </React.Fragment>
     }
 }
