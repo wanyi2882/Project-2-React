bdb551c612ac428cfa2832d791edc3ee323c9848
wanyi2882 <wanyi.2882@gmail.com>
2021-10-22 08:52:43 +0000

    Changed what is displayed and active pages

--- project-2/src/components/Florists.js
+++ project-2/src/components/Florists.js
@@ -1,40 +1,39 @@
 import React from 'react'
-import axios from 'axios'
 import LoginFlorists from './LoginFlorists'
 import AddFlorists from './AddFlorists'
-import AddListing from './AddListing'
 
-
-export default class Florists extends React.Component{
+export default class Florists extends React.Component {
 
     state = {
-        'active': 'loginFlorists'
+        'active': 'loginFlorist'
     }
 
     setActive = (page) => {
         this.setState({
-            active: page
+            'active': page,
         })
     }
 
+    afterAddFlorist = () => {
+        this.setActive("loginFlorist")
+    }
+
+    goToCreateProfile = () => {
+        this.setActive("addFlorist")
+    }
+
     renderContent() {
-        if (this.state.active == 'loginFlorists') {
-            return <LoginFlorists />
-        } else if (this.state.active == 'AddFlorists') {
-            return <AddFlorists />
-        }  else if (this.state.active == "AddListing"){
-            return <AddListing />
+        if (this.state.active == 'loginFlorist') {
+            return <LoginFlorists onGoToCreateProfile={this.goToCreateProfile}/>
+        } else if (this.state.active == 'addFlorist') {
+            return <AddFlorists onAfterAddFlorist={this.afterAddFlorist}/>
         }
     }
 
-    render(){
+    render() {
         return <React.Fragment>
-                <button onClick={()=>this.setActive('AddFlorists')}>Create new Florist Account</button>
-                <button onClick={()=>this.setActive('loginFlorists')}>See your Florist Profile</button>
-                <button onClick={()=>this.setActive('AddListing')}>Add new Listing</button>
-                <button>See all your Listings</button>
-                {this.renderContent()}      
-                </React.Fragment>
+            {this.renderContent()}
+        </React.Fragment>
     }
 
 }
