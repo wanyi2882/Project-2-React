c6ec6568945542930eb59fe7f606de94677ce226
wanyi2882 <wanyi.2882@gmail.com>
2021-10-28 10:29:59 +0000

    Created Delete Florists page

---
+++ project-2/src/components/DeleteFlorists.js
@@ -0,0 +1,54 @@
+import React from "react";
+import axios from "axios";
+
+export default class DeleteFlorists extends React.Component {
+
+    url = 'https://3000-tan-nightingale-xhc8uhmi.ws-us18.gitpod.io'
+
+    state = {
+        'floristIdToDelete': this.props.florist_id,
+        'floristEmailToDelete': this.props.florist_email,
+        'floristConfirmEmail': ""
+    }
+
+    updateFormField = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    deleteProfile = async () => {
+
+        if (this.state.floristEmailToDelete == this.state.floristConfirmEmail){
+            await axios.delete(this.url + "/florists/" + this.state.floristIdToDelete)
+
+            alert("Profile successfully deleted.")
+
+            this.props.afterDeleteProfile()
+
+
+        } else {
+            alert("The email address you have entered does not match.")
+        }
+
+    }
+
+
+    render() {
+        return <React.Fragment>
+            <div>Please confirm if you want to delete your profile by enter your email below.</div>
+            <div>Please note that all your listings under your profile will be deleted.</div>
+
+            <div>
+                <label className="form-label">Email Address: </label>
+                <input type="text"
+                    className="form-control"
+                    name="floristConfirmEmail"
+                    value={this.state.floristConfirmEmail}
+                    onChange={this.updateFormField} />
+            </div>
+            <div><button className="btn btn-danger"
+                onClick={() => this.deleteProfile()}>Confirm Delete Profile</button></div>
+        </React.Fragment>
+    }
+}
--- project-2/src/components/LoginFlorists.js
+++ project-2/src/components/LoginFlorists.js
@@ -3,10 +3,11 @@ import axios from 'axios'
 import EditFlorists from './EditFlorists'
 import FloristViewListings from './FloristViewListings'
 import AddListing from './AddListing'
+import DeleteFlorists from './DeleteFlorists'
 
 export default class LoginFlorists extends React.Component {
 
-    url = 'https://3000-tan-nightingale-xhc8uhmi.ws-us17.gitpod.io'
+    url = 'https://3000-tan-nightingale-xhc8uhmi.ws-us18.gitpod.io'
 
     state = {
         'data': [
@@ -28,7 +29,8 @@ export default class LoginFlorists extends React.Component {
         'editContactNumber': "",
         'floristViewListings': false,
         'floristViewListingsId': "",
-        'addListing': false
+        'addListing': false,
+        'deleteFlorist': false
     }
 
     fetchData = async () => {
@@ -47,22 +49,29 @@ export default class LoginFlorists extends React.Component {
         this.fetchData()
     }
 
+    // After clicking the see profile button, if async function promise is successful, 
+    // it will alert succesful login and display the profile of florist. 
+    // Else, it will alert user to check their login in details, page remains at login.
+
     seeProfileBtn = () => {
         this.fetchData().then(
-            this.action1,this.action2
-        )}
+            this.action1, this.action2
+        )
+    }
 
     action1 = () => {
         this.setState({
             'displayProfile': true,
             'displayLogin': false
         })
-        alert("Successful")        
+        alert("Successful")
     }
 
     action2 = () => {
         alert("Check Again")
     }
+
+    // Update form fields 2 way binding
 
     updateFormField = (event) => {
         this.setState({
@@ -70,9 +79,15 @@ export default class LoginFlorists extends React.Component {
         })
     }
 
-    createProfileBtn = () =>{
+    // Create a new florist profile button.
+    // Renders AddFlorists.js from Florists.js via props.
+
+    createProfileBtn = () => {
         this.props.onGoToCreateProfile()
     }
+
+
+    // Displays the login form
 
     displayLogin = () => {
         if (this.state.displayLogin) {
@@ -97,16 +112,20 @@ export default class LoginFlorists extends React.Component {
                     </div>
                     <div>
                         <button className="btn btn-primary"
-                                onClick={() => this.seeProfileBtn()}>See your profile</button>
+                            onClick={() => this.seeProfileBtn()}>See your profile</button>
                         <button className="btn btn-danger"
-                                onClick={() => this.createProfileBtn()}>New? Create A New Profile</button>
+                            onClick={() => this.createProfileBtn()}>New? Create A New Profile</button>
                     </div>
                 </div>
             </React.Fragment>
         }
     }
 
-    EditForm = (each) => {
+    // Sets the states for when edit button is clicked, to be sent to EditFlorists.js.
+    // Edit florist information form.
+    // Only 'displayEdit' is true. Other pages to be false.
+
+    editForm = (each) => {
         this.setState({
             'displayEdit': true,
             'profileBeingEdited': each,
@@ -119,17 +138,14 @@ export default class LoginFlorists extends React.Component {
             'editFacebookURL': each.contact.facebook,
             'editContactNumber': each.contact.number,
             'floristViewListings': false,
-            'addListing': false
+            'addListing': false,
+            'deleteFlorist': false
         })
     }
 
-    FloristViewListings = (each) =>{
-        this.setState({
-            'floristViewListings': true,
-            'displayEdit': false,
-            'addListing': false
-        })
-    }
+    // EditFlorists.js editing confirmation, bring florist to login page again.
+    // Set all states for under EditFlorists to be empty.
+    // 'displayLogin' as true, all other pages false.
 
     afterConfirmEditFlorist = () => {
         this.setState({
@@ -137,7 +153,7 @@ export default class LoginFlorists extends React.Component {
             'displayLogin': true,
             'displayProfile': false,
             'loginUserName': "",
-            'loginEmail': "",        
+            'loginEmail': "",
             'profileBeingEdited': {},
             'editFloristId': "",
             'editUsername': "",
@@ -148,56 +164,113 @@ export default class LoginFlorists extends React.Component {
             'editFacebookURL': "",
             'editContactNumber': ""
         })
-
     }
 
-    AddListingForm = () => {
-        console.log(this.state.data[0])
+    // sets the states for the when the delete profile button is clicked, 
+    // to be sent to DeleteFlorists.js.
+
+    deleteForm = () => {
         this.setState({
+            'deleteFlorist': true,
+            'addListing': false,
             'floristViewListings': false,
-            'displayEdit': false,
-            'addListing': true
+            'displayEdit': false
         })
     }
 
+    // Florists can view all the listing under them
+    // Only 'floristViewListings' is true, the other pages to be false.
+
+    floristViewListings = () => {
+        this.setState({
+            'floristViewListings': true,
+            'displayEdit': false,
+            'addListing': false,
+            'deleteFlorist': false
+        })
+    }
+
+    // Florist add new listing, addListingForm sets 'addListing' page to true.
+    // Other pages to be false.
+
+    addListingForm = () => {
+        console.log(this.state.data[0])
+        this.setState({
+            'addListing': true,
+            'floristViewListings': false,
+            'displayEdit': false,
+            'deleteFlorist': false
+        })
+    }
+
+    // After deleting florist profile, go back to main login page.
+
+    onAfterDeleteFlorist = () => {
+        this.setState({
+            'displayLogin': true,
+            'deleteFlorist': false,
+            'displayProfile': false,
+            'floristViewListingsId': "",
+            'loginUserName': "",
+            'loginEmail': ""
+        })
+    }
+
+
+    // Render different forms 
+
     displayForms = () => {
-        if (this.state.displayEdit == true){
-            return < EditFlorists 
-                    modifiedUsername = {this.state.editUsername}
-                    modifiedLoginEmail = {this.state.editLoginEmail}
-                    modifiedFloristName = {this.state.editFloristName}
-                    modifiedContactCategory = {this.state.editContactCategory}
-                    modifiedInstagramURL = {this.state.editInstagramURL}
-                    modifiedFacebookURL = {this.state.editFacebookURL}
-                    modifiedContactNumber = {this.state.editContactNumber}
-                    modifiedFloristId = {this.state.editFloristId}
-                    updateFormField = {this.updateFormField}
-                    afterConfirmEditFlorist = {this.afterConfirmEditFlorist}/>
-        } else if (this.state.floristViewListings == true){
-            return < FloristViewListings 
-                    floristViewListingsId = {this.state.floristViewListingsId}/>
-        } else if (this.state.addListing == true){
-            return <AddListing 
-            florist_id = {this.state.data[0]._id}
-            florist_name = {this.state.data[0].name}
-            number = {this.state.data[0].contact.number}
-            instagram = {this.state.data[0].contact.instagram}
-            facebook = {this.state.data[0].contact.facebook}
-            contact_method = {this.state.data[0].contact_method}
-            onAfterAddListing = {this.FloristViewListings}/>
+        if (this.state.displayEdit == true) {
+            // Displays EditFlorists.js Form
+            return < EditFlorists
+                modifiedUsername={this.state.editUsername}
+                modifiedLoginEmail={this.state.editLoginEmail}
+                modifiedFloristName={this.state.editFloristName}
+                modifiedContactCategory={this.state.editContactCategory}
+                modifiedInstagramURL={this.state.editInstagramURL}
+                modifiedFacebookURL={this.state.editFacebookURL}
+                modifiedContactNumber={this.state.editContactNumber}
+                modifiedFloristId={this.state.editFloristId}
+                updateFormField={this.updateFormField}
+                afterConfirmEditFlorist={this.afterConfirmEditFlorist} />
+
+        } else if (this.state.floristViewListings == true) {
+            // Displays FloristsViewListings.js page
+            return < FloristViewListings
+                floristViewListingsId={this.state.floristViewListingsId} />
+
+        } else if (this.state.addListing == true) {
+            // Displays AddListing.js Form
+            return <AddListing
+                florist_id={this.state.data[0]._id}
+                florist_name={this.state.data[0].name}
+                number={this.state.data[0].contact.number}
+                instagram={this.state.data[0].contact.instagram}
+                facebook={this.state.data[0].contact.facebook}
+                contact_method={this.state.data[0].contact_method}
+                onAfterAddListing={this.floristViewListings} />
+
+        } else if (this.state.deleteFlorist == true) {
+            // Display DeleteFlorists.js
+            return <DeleteFlorists
+                florist_id={this.state.data[0]._id}
+                florist_email={this.state.data[0].login_email}
+                afterDeleteProfile={this.onAfterDeleteFlorist} />
         }
     }
 
+    // After successful login, display profile of florists and the various actions
+    // that florist can do. (Eg. Edit profile, add/edit/delete listings, delete profile)
     showProfile = () => {
         if (this.state.displayProfile) {
             return <React.Fragment>
                 <div>{this.state.data.map(each =>
                     <div key={each._id}>
                         <h1>Welcome Back {each.name} !</h1>
-                        <button onClick={() => this.EditForm(each)}>Edit Your Profile</button>
-                        <button onClick={() => this.AddListingForm()}>Add New Listing</button>
-                        <button onClick={() => this.FloristViewListings(each)}>View Your Listings</button>
-                        <button>Delete Your Profile</button>
+                        <button onClick={() => this.editForm(each)}>Edit Your Profile</button>
+                        <button onClick={() => this.addListingForm()}>Add New Listing</button>
+                        <button onClick={() => this.floristViewListings()}>View Your Listings</button>
+                        <button onClick={() => this.deleteForm(each)}>Delete Your Profile</button>
                     </div>)}
                 </div>
                 {this.displayForms()}
@@ -205,6 +278,7 @@ export default class LoginFlorists extends React.Component {
         }
     }
 
+    // Render the various pages according to the states
     render() {
         return <React.Fragment>
             {this.displayLogin()}
