0749d466623efcb07162930ea8aa07a27a09167b
wanyi2882 <wanyi.2882@gmail.com>
2021-10-29 15:48:46 +0000

    Collapsed Navbar functioning

--- src/Main.js
+++ src/Main.js
@@ -10,17 +10,31 @@ export default class FloristStop extends React.Component {
 
 
     state = {
-        'active': 'home'
+        'active': 'home',
+        'navDropdown': false
     }
 
     setActive = (page) => {
         this.setState({
-            active: page
+            'active': page,
+            'navDropdown': false
         })
     }
 
-    afterAddListing = () =>{
+    afterAddListing = () => {
         this.setActive('listings')
+    }
+
+    toggleNav = () => {
+        if (this.state.navDropdown == false) {
+            this.setState({
+                navDropdown: true
+            })
+        } else {
+            this.setState({
+                navDropdown: false
+            })
+        }
     }
 
     renderContent() {
@@ -31,18 +45,18 @@ export default class FloristStop extends React.Component {
         } else if (this.state.active == 'florists') {
             return <Florists />
         } else if (this.state.active == 'admin') {
-            return <Admin onAfterAddListing={this.afterAddListing}/>
+            return <Admin onAfterAddListing={this.afterAddListing} />
         }
     }
 
     render() {
         return (
             <React.Fragment>
-                <div id="nav-bar-fonts" className="container-fluid">
+                {/* Nav Bar when screen size more than 576px */}
+                <div id="navbar-expand" className="container-fluid fixed-top">
                     <ul className="nav nav-tabs justify-content-end">
                         <li className="nav-item">
                             <button className="nav-link"
-                                aria-current="page"
                                 onClick={() => this.setActive('home')}>Home</button>
                         </li>
                         <li className="nav-item">
@@ -60,7 +74,61 @@ export default class FloristStop extends React.Component {
                     </ul>
                 </div>
 
+                {/* Nav Bar when screen size less than 576px */}
+                <nav id="collapsed-navbar" className="navbar navbar-light fixed-top">
+                    <div className="container-fluid">
+                        <div className="navbar-brand" role="button"></div>
+                        <button className="navbar-toggler"
+                            type="button"
+                            data-bs-toggle="offcanvas"
+                            data-bs-target="#offcanvasNavbar"
+                            aria-controls="offcanvasNavbar"
+                            onClick={() => { this.toggleNav()}}>
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
 
+                        {this.state.navDropdown ? 
+                        <div className="offcanvas offcanvas-end"
+                            tabindex="-1" 
+                            id="offcanvasNavbar"
+                            aria-labelledby="offcanvasNavbarLabel">
+
+                            <div className="offcanvas-header">
+                                <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+                                <button type="button"
+                                    className="btn-close text-reset"
+                                    data-bs-dismiss="offcanvas"
+                                    aria-label="Close"
+                                    onClick={() => { this.toggleNav()}}></button>
+                            </div>
+
+                            <div className="offcanvas-body">
+                                <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                                    <li className="nav-item">
+                                        <div className="nav-link"
+                                            onClick={() => this.setActive('home')}>Home</div>
+                                    </li>
+                                    <li className="nav-item">
+                                        <div className="nav-link"
+                                            onClick={() => this.setActive('admin')}>Admin</div>
+                                    </li>
+                                    <li className="nav-item">
+                                        <div className="nav-link"
+                                            onClick={() => this.setActive('listings')}>Listings</div>
+                                    </li>
+                                    <li className="nav-item">
+                                        <div className="nav-link"
+                                            onClick={() => this.setActive('florists')}>Florists</div>
+                                    </li>
+
+                                </ul>
+                            </div>
+                        </div>
+                        : null
+                    }
+
+                    </div>
+                </nav>
 
                 {this.renderContent()}
             </React.Fragment>
