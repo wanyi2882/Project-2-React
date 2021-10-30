5eef722cd1cbec5bf046c1f63b0a29959bece4ec
wanyi2882 <wanyi.2882@gmail.com>
2021-10-30 10:07:27 +0000

    Css for home.js and linking to listing and florist

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,12 +8,42 @@ export default class Listing extends React.Component {
 
     }
 
+    goToListings = () => {
+        this.props.goToListings()
+    }
 
+    goToFlorists = () => {
+        this.props.goToFlorists()
+    }
 
-    render(){
+    render() {
         return <React.Fragment>
-            <div className="home-background">
-                <div>A marketplace for </div>
+            <div id="home-background">
+                <div className="container">
+                    <div className="row">
+                        <div id="introduction" className="col">
+                            <div>Floral Market Place</div>
+                            <div>“Where flowers bloom, so does hope.” – Lady Bird Johnson</div>
+                        </div>
+                    </div>
+
+                    <div id="introduction-two" className="row justify-content-around">
+                        <div id="viewAllListings" 
+                        className="col-12 col-sm-5"
+                        role="button"
+                        onClick={() =>{this.goToListings()}}>
+                            <span>Looking for a bouquet?</span> <br />
+                            <span>Click here to view all our listings! </span>
+                        </div>
+                        <div id="floristPage" 
+                        className="col-12 col-sm-5"
+                        role="button"
+                        onClick={() =>{this.goToFlorists()}}>
+                            <span>Freelance or Homebased florists?</span> <br />
+                            <span>Click here to start listing with us!</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </React.Fragment>
     }
