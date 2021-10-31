d5957911298bb2412e2c96d045264381d887c731
wanyi2882 <wanyi.2882@gmail.com>
2021-10-31 07:28:53 +0000

    amended css and sort listings viewed by florist from latest listing to oldest

--- src/components/FloristViewListings.js
+++ src/components/FloristViewListings.js
@@ -3,8 +3,9 @@ import axios from 'axios'
 import Moment from 'react-moment';
 import EditListing from './EditListing'
 import DeleteListing from './DeleteListing'
+import '../components-css/FloristViewListings.css'
 
-export default class FloristViewListings extends React.Component{
+export default class FloristViewListings extends React.Component {
 
     // Deployment URL
     // url = 'https://ywy-project2-fmp-express-app.herokuapp.com'
@@ -35,8 +36,16 @@ export default class FloristViewListings extends React.Component{
     fetchData = async () => {
         let response = await axios.get(this.url + "/listings")
 
+        // Sort by descending date (Default)                                         
+        let array = response.data
+        array.sort(function (a, b) {
+            if (a.date_listed < b.date_listed) return 1;
+            if (a.date_listed > b.date_listed) return -1;
+            return 0;
+        })
+
         this.setState({
-            'data': response.data
+            'data': array
         })
 
         //console.log(response.data)
@@ -77,8 +86,7 @@ export default class FloristViewListings extends React.Component{
         this.setState({
             'displayDeleteModalBox': true,
             'listingToBeEdited': listing,
-            'listingIdToBeDeleted': listing._id,
-
+            'listingIdToBeDeleted': listing._id
         })
     }
 
@@ -89,52 +97,54 @@ export default class FloristViewListings extends React.Component{
         this.setState({
             'active': "displayViewAllListings",
             'displayDeleteModalBox': false
-        })        
+        })
     }
 
     // Displays the Edit Modal Box (EditListing.js) or Delete Modal Box (DeleteListing.js)
     renderModal() {
         if (this.state.displayEditModalBox) {
             // Information passed via props to EditListing.js
-            return <EditListing 
-                    displayViewAllListings = {this.onAfterEditListings}
-                    listingToBeEdited = {this.state.listingToBeEdited}
-                    listingIdToBeEdited = {this.state.listingIdToBeEdited}
-                    listingNameToBeEdited = {this.state.listingNameToBeEdited}
-                    listingDescriptionToBeEdited = {this.state.listingDescriptionToBeEdited}
-                    listingFlowerTypeToBeEdited = {this.state.listingFlowerTypeToBeEdited}
-                    listingPriceToBeEdited = {this.state.listingPriceToBeEdited}
-                    listingOccasionToBeEdited = {this.state.listingOccasionToBeEdited}
-                    listingQuantityToBeEdited = {this.state.listingQuantityToBeEdited}
-                    listingImageToBeEdited = {this.state.listingImageToBeEdited}/>
+            return <EditListing
+                displayViewAllListings={this.onAfterEditListings}
+                listingToBeEdited={this.state.listingToBeEdited}
+                listingIdToBeEdited={this.state.listingIdToBeEdited}
+                listingNameToBeEdited={this.state.listingNameToBeEdited}
+                listingDescriptionToBeEdited={this.state.listingDescriptionToBeEdited}
+                listingFlowerTypeToBeEdited={this.state.listingFlowerTypeToBeEdited}
+                listingPriceToBeEdited={this.state.listingPriceToBeEdited}
+                listingOccasionToBeEdited={this.state.listingOccasionToBeEdited}
+                listingQuantityToBeEdited={this.state.listingQuantityToBeEdited}
+                listingImageToBeEdited={this.state.listingImageToBeEdited} />
 
         } else if (this.state.displayDeleteModalBox) {
             // Information passed via props to DeleteListing.js
-            return <DeleteListing 
-                    displayViewAllListings = {this.onAfterDeleteListings}
-                    listingIdToBeDeleted = {this.state.listingIdToBeDeleted}
-                    floristProfile = {this.state.floristProfile}
+            return <DeleteListing
+                displayViewAllListings={this.onAfterDeleteListings}
+                listingIdToBeDeleted={this.state.listingIdToBeDeleted}
+                floristProfile={this.state.floristProfile}
             />
         } else {
             return null;
         }
     }
 
-    render(){
+    render() {
         return <React.Fragment>
-            <h1>View your listings</h1>
-            {this.state.data.map(listing => 
-            (this.state.floristProfile[0]._id == listing.florist.florist_id) ? 
-            <div>
-                <h6>{listing.name} dated <Moment format="D MMM YYYY">{listing.date_listed}</Moment></h6>
-                <button onClick={() => {this.diplayEditModalBox(listing)}}>Edit Listing</button>
-                <button onClick={() => {this.displayDeleteModalBox(listing)}}>Delete Listing</button>
+            <div className="container">
+                <h1>View your listings</h1>
+                {this.state.data.map(listing =>
+                    (this.state.floristProfile[0]._id == listing.florist.florist_id) ?
+                        <div className="view-listing-div">
+                            <h6>{listing.name} dated <Moment format="D MMM YYYY">{listing.date_listed}</Moment></h6>
+                            <button className="btn btn-primary btn-sm mx-2" onClick={() => { this.diplayEditModalBox(listing) }}>Edit Listing</button>
+                            <button className="btn btn-danger btn-sm" onClick={() => { this.displayDeleteModalBox(listing) }}>Delete Listing</button>
+                        </div>
+                        : null
+                )}
             </div>
-            : null
-        )}
 
-        <div>{this.renderModal()}</div>
-        
+            <div>{this.renderModal()}</div>
+
         </React.Fragment>
     }
 }
